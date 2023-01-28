736260d69d61deb8a09d94b8a651d0e19410bbea
Ahmed Belloula <belloulaahmed7@gmail.com>
2023-01-28 22:10:50 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+const About = () => {
+    return ( 
+        <>
+    <Head>
+      <title>Ninja List | Home</title>
+      <meta name="keywords" content="ninjas" />
+    </Head>
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+        </div>
+
+        </>
+     );
+}
+ 
+export default About;
